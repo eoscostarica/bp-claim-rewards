4561456d917f190d4e9007fdb55c39039f35ccfc
Xavier Fernandez <xavier@edenia.com>
2022-08-23 08:50:02 -0600

    fix(claim): update env var names

--- src/config/eos.config.js
+++ src/config/eos.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  endpoint: process.env.EOS_API_ENDPOINT,
-  chainId: process.env.EOS_API_CHAIN_ID,
-  bpAccount: process.env.EOS_BP_ACCOUNT,
-  claimPerms: process.env.EOS_CLAIM_PERMISION,
-  claimKey: process.env.EOS_CLAIM_PRIVATE_KEY
+  endpoint: process.env.CLAIM_API_ENDPOINT,
+  chainId: process.env.CLAIM_API_CHAIN_ID,
+  bpAccount: process.env.CLAIM_BP_ACCOUNT,
+  claimPerms: process.env.CLAIM_PERMISION,
+  claimKey: process.env.CLAIM_PRIV_KEY
 }
